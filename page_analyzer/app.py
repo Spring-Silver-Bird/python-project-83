7d4f1cff1db429c5d28127730cd65461baeb3eac
Spring-Silver-Bird <adelaida_19@mail.ru>
2025-04-29 22:44:33 +0300

    apdate files for deploy on render.com

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,7 +1,10 @@
+from dotenv import load_dotenv
+import os
 from flask import Flask
 
+load_dotenv()
 app = Flask(__name__)
-
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 
 @app.route('/')
 def hello():
